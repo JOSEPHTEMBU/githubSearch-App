21b4d3011c1227053bb583f4d4ed4d28094568f6
JOSEPHTEMBU <josephmulama30@gmail.com>
2020-10-04 00:42:37 +0300

    generate search form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { GithubComponent } from './github/github.component';
+import { SearchFormComponent } from './search-form/search-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     GithubComponent,
+    SearchFormComponent,
     
   ],
   imports: [
