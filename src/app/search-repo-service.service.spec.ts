0dc264cf30dfa015b845429e69d4d2f20ea5b8d3
JOSEPHTEMBU <josephmulama30@gmail.com>
2020-10-04 01:07:50 +0300

    generate service search

---
+++ src/app/search-repo-service.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { SearchRepoServiceService } from './search-repo-service.service';
+
+describe('SearchRepoServiceService', () => {
+  let service: SearchRepoServiceService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(SearchRepoServiceService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
