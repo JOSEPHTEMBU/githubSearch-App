52bc6b54358a07c1ca00e0fb7887cf6bb873047f
JOSEPHTEMBU <josephmulama30@gmail.com>
2020-10-04 09:41:07 +0300

    updated home component html and added app-navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,67 +1,67 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { GithubComponent } from './github/github.component';
-import { SearchFormComponent } from './search-form/search-form.component';
-import { TimeCountPipe } from './time-count.pipe';
-import { NavbarComponent } from './navbar/navbar.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { UserDetailsComponent } from './user-details/user-details.component';
-import { UserRepositoryComponent } from './user-repository/user-repository.component';
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    GithubComponent,
-    SearchFormComponent,
-    TimeCountPipe,
-    NavbarComponent,
-    NotFoundComponent,
-    UserDetailsComponent,
-    UserRepositoryComponent,
-
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
 // import { BrowserModule } from '@angular/platform-browser';
 // import { NgModule } from '@angular/core';
 // import { HttpClientModule } from '@angular/common/http';
 // import { AppRoutingModule } from './app-routing.module';
 // import { AppComponent } from './app.component';
+// import { HomeComponent } from './home/home.component';
 // import { GithubComponent } from './github/github.component';
 // import { SearchFormComponent } from './search-form/search-form.component';
+// import { TimeCountPipe } from './time-count.pipe';
 // import { NavbarComponent } from './navbar/navbar.component';
-// import { HomeComponent } from './home/home.component';
-// import { DateCountPipe } from './date-count.pipe';
-// import { FormsModule } from '@angular/forms';
-// import { UserDetailsComponent } from './user-details/user-details.component';
 // import { NotFoundComponent } from './not-found/not-found.component';
-// import { UserReposComponent } from './user-repos/user-repos.component';
+// import { UserDetailsComponent } from './user-details/user-details.component';
+// import { UserRepositoryComponent } from './user-repository/user-repository.component';
 
 // @NgModule({
 //   declarations: [
 //     AppComponent,
+//     HomeComponent,
 //     GithubComponent,
 //     SearchFormComponent,
+//     TimeCountPipe,
 //     NavbarComponent,
-//     HomeComponent,
-//     DateCountPipe,
-//     UserDetailsComponent,
 //     NotFoundComponent,
-//     UserReposComponent,
+//     UserDetailsComponent,
+//     UserRepositoryComponent,
+//     HttpClientModule
 //   ],
-//   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule
+//   ],
 //   providers: [],
-//   bootstrap: [AppComponent],
+//   bootstrap: [AppComponent]
 // })
-// export class AppModule {}
+// export class AppModule { }
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GithubComponent } from './github/github.component';
+import { SearchFormComponent } from './search-form/search-form.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { TimeCountPipe } from './time-count.pipe';
+import { FormsModule } from '@angular/forms';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { UserRepositoryComponent } from './user-repository/user-repository.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GithubComponent,
+    SearchFormComponent,
+    NavbarComponent,
+    HomeComponent,
+    TimeCountPipe,
+    UserDetailsComponent,
+    NotFoundComponent,
+    UserRepositoryComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
